53016ed7439d945fc2116e290712db6c9298100e
Sandor Maro <marosandor2001@gmail.com>
2024-10-18 23:17:45 +0200

    Inheritance finish
    6. előadás anyaga: Öröklődés. Inheritance => Main, Szakacs, MagyarSzakacs osztályok.

---
+++ University/Eloadas/Inheritance/src/MagyarSzakacs.java
@@ -0,0 +1,10 @@
+public class MagyarSzakacs extends Szakacs {
+    public void keszitPorkolt() {
+        System.out.println("Pörkölt készítése...");
+    }
+
+    @Override
+    public void keszitSpecialitas() {
+        System.out.println("Hortobágyi húsospalacsinta készítése..."); // Szakács specialitása
+    }
+}
---
+++ University/Eloadas/Inheritance/src/Szakacs.java
@@ -0,0 +1,13 @@
+public class Szakacs {
+    public void keszitLeves () {
+        System.out.println("Leves készítése...");
+    }
+
+    public void keszitFoetel() {
+        System.out.println("Főétel készítése...");
+    }
+
+    public void keszitSpecialitas() {
+        System.out.println("Vadasmarha készítése..."); // Szakács specialitása
+    }
+}

3f7b31d65bfb3c3bd5689c3b5859345d328c73d9
Sandor Maro <marosandor2001@gmail.com>
2024-10-18 19:58:07 +0200

    Gyakorlat 4 finish
    4. gyakorlat anyaga. Csaszar és Fan konzolos programok. Konstruktor, getter/setter metódusok, List használata, Scanner használata soronként/splittelve.

---
+++ University/Gyakorlat/Gyakorlat-4/src/Fan/Main.java
@@ -0,0 +1,24 @@
+package Fan;
+
+public class Main {
+    public static void main(String[] args) {
+        Fan f = new Fan();
+        f.setColor("red");
+        f.setSpeed(Fan.MEDIUM);
+        f.setOn(true);
+
+        Fan f2 = new Fan();
+        f2.setOn(true);
+
+        Fan f3 = new Fan();
+        f3.setColor("red");
+        f3.setSpeed(Fan.MEDIUM);
+        f3.setOn(true);
+
+        System.out.println(f);
+        System.out.println(f2);
+        System.out.println(f3);
+        System.out.println("Egyenlő-e a két ventillátor: " + f.equals(f2));
+        System.out.println("Egyenlő-e a két ventillátor: " + f.equals(f3));
+    }
+}

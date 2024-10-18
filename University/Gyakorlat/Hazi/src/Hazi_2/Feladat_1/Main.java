0ef101fa0f0a97d5a7d2709f55aeaa98b7969a74
Sandor Maro <marosandor2001@gmail.com>
2024-10-18 23:44:30 +0200

    Hazi 2 finish
    Pont és Main osztályok. Pont osztály és annak metódusai. LinearEquation és Main osztályok és annak metódusai. Rectangle és Main osztályok és annak metódusai.

---
+++ University/Gyakorlat/Hazi/src/Hazi_2/Feladat_1/Main.java
@@ -0,0 +1,20 @@
+package Hazi_2.Feladat_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Pont p1 = new Pont(5, 10);
+        Pont p2 = new Pont(10, 15);
+        Pont p3 = new Pont(15, 20);
+        Pont p4 = new Pont(20, 25);
+
+        p1.setY(p1.getY() + 5);
+        p2.setY(p2.getY() + 5);
+        p3.setX(p3.getX() - 3.4);
+        p4.setX(p4.getX() - 3.4);
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.println(p4);
+    }
+}

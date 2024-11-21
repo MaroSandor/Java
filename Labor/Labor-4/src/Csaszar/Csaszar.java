d9ae418782f21c1a1afced9cdd7b2edf5661fd3b
Sandor Maro <marosandor2001@gmail.com>
2024-11-21 20:43:22 +0100

    Create Labor-4/Csaszar/Csaszar.java

---
+++ Labor/Labor-4/src/Csaszar/Csaszar.java
@@ -0,0 +1,32 @@
+package Csaszar;
+
+public class Csaszar {
+    private String name;
+    private int age;
+
+    public Csaszar(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBirthOfYear() {
+        return age;
+    }
+
+    public void setBirthOfYear(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " (" + this.age + ')';
+    }
+}

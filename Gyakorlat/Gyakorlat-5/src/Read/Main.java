daffab0cf2778d1f330614fdc62a7ffbcc66a548
Sandor Maro <marosandor2001@gmail.com>
2024-11-25 13:13:09 +0100

    Create Main.java

---
+++ Gyakorlat/Gyakorlat-5/src/Read/Main.java
@@ -0,0 +1,17 @@
+package Read;
+
+import java.util.List;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String fname = "input.txt";
+        List<String> sorok = FileUtils.readLines(fname);
+
+        for (String sor : sorok) {
+            System.out.println(sor);
+        }
+    }
+}
+

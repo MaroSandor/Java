776a9441d50b7ba82465a4b7c378b03ad0b7682b
Sandor Maro <marosandor2001@gmail.com>
2024-11-25 13:13:04 +0100

    Create Main.java

---
+++ Gyakorlat/Gyakorlat-5/src/StrRev/Main.java
@@ -0,0 +1,17 @@
+package StrRev;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String s = "Java is cool!";
+
+        String vissza = StringUtils.strrev(s);
+
+        System.out.println(s);
+        System.out.println(vissza);
+
+        System.out.println(StringUtils.isPalindrome("abbad"));
+    }
+}
+

8c98a8bda200d869f2d35d556b1a605675a703a5
Sandor Maro <marosandor2001@gmail.com>
2024-11-25 13:12:52 +0100

    Create Main.java

---
+++ Gyakorlat/Gyakorlat-6/src/Matrixok/Main.java
@@ -0,0 +1,22 @@
+package Matrixok;
+
+import java.util.Arrays;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // int[][] matrix = new int[3][2];    // kinullÃ¡zza a mÃ¡trixot
+        // matrix[0][0] = 2;
+        // matrix[0][1] = 3;
+        // ...
+
+        int[][] matrix = {
+                {2, 3},
+                {5, 8},
+                {7, 1},
+        };
+
+        System.out.println(Arrays.deepToString(matrix));
+    }
+}

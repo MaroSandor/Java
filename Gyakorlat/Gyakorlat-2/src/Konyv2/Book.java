dc97f49e3f1280890b5a84a8d2136b61c8bd7b22
Sandor Maro <marosandor2001@gmail.com>
2024-11-25 12:20:22 +0100

    Created Konyv2/Book.java

---
+++ Gyakorlat/Gyakorlat-2/src/Konyv2/Book.java
@@ -0,0 +1,13 @@
+package Konyv2;
+
+public class Book {
+    public String title;
+    public String author;
+    public int pages;
+
+    public Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+}

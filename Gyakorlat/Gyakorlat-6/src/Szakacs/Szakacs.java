8e0ec22ab8520b1a445f9e2bf1a2938caba1c6be
Sandor Maro <marosandor2001@gmail.com>
2024-11-25 13:12:44 +0100

    Create Szakacs.java

---
+++ Gyakorlat/Gyakorlat-6/src/Szakacs/Szakacs.java
@@ -0,0 +1,16 @@
+package Szakacs;
+
+public class Szakacs
+{
+    public void keszitLeves() {
+        System.out.println("leves keszitese");
+    }
+
+    public void keszitFoetel() {
+        System.out.println("foetel keszitese");
+    }
+
+    public void keszitSpecialitas() {
+        System.out.println("vadas marha keszitese");
+    }
+}

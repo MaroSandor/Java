f99767a8f1c9bbfcb6c6a6797017929cf2d4f47c
Sandor Maro <marosandor2001@gmail.com>
2024-12-04 13:39:35 +0100

    Update Main.java

--- Projekt/src/Main.java
+++ Projekt/src/Main.java
@@ -25,7 +25,7 @@ public class Main {
         }
     }
 
-    public static boolean isSupportedImage(File file) {
+    public static boolean isImage(File file) {
         String fileName = file.getName().toLowerCase();
         return fileName.endsWith(".jpg") || fileName.endsWith(".jpeg") || fileName.endsWith(".png") || fileName.endsWith(".gif");
     }

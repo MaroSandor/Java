d271fb3df3b3723b467b2200c67f1ba3de36e624
Sandor Maro <marosandor2001@gmail.com>
2024-12-04 13:39:06 +0100

    Create Image.java

---
+++ HTML_Generator/src/Image.java
@@ -0,0 +1,21 @@
+public class Image {
+    private String fileName;
+    private String filePath;
+
+    public Image(String fileName, String filePath) {
+        this.fileName = fileName;
+        this.filePath = filePath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public String getHtmlFileName() {
+        return fileName.substring(0, fileName.lastIndexOf(".")) + ".html";
+    }
+}
